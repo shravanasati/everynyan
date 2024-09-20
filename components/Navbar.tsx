52ed293342c3b72091d111dcc40979d9c81510f3
ni3rav <mahetan35@gmail.com>
2024-09-20 16:19:00 +0530

    minor fixes in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,11 +11,11 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-zinc-900 shadow-md z-50 fixed min-w-[50%] mt-8  rounded-2xl">
+    <nav className="bg-zinc-900 shadow-md z-50 fixed sm:min-w-[50%] mt-8  rounded-2xl min-w-[80%]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-xl font-extrabold text-zinc-200">
+            <Link href="/" className="text-3xl font-black text-zinc-50">
               Everynyan
             </Link>
           </div>
@@ -23,25 +23,25 @@ export default function Navbar() {
             <div className="ml-10 flex items-baseline space-x-4">
               <Link
                 href="/"
-                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-xl font-medium"
               >
                 Home
               </Link>
               <Link
                 href="/login"
-                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-xl font-medium"
               >
                 LogIn
               </Link>
               <Link
                 href="/tnc"
-                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-xl font-medium"
               >
                 TnC
               </Link>
               <Link
                 href="/contact"
-                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-sm font-medium"
+                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-xl font-medium"
               >
                 Contact
               </Link>
@@ -68,25 +68,25 @@ export default function Navbar() {
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
             <Link
               href="/"
-              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-xl font-medium"
             >
               Home
             </Link>
             <Link
               href="/login"
-              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-xl font-medium"
             >
               LogIn
             </Link>
             <Link
               href="/tnc"
-              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-xl font-medium"
             >
               TnC
             </Link>
             <Link
               href="/contact"
-              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-base font-medium"
+              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-xl font-medium"
             >
               Contact
             </Link>
