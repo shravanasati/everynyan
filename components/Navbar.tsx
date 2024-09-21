2b7e34a7beb045673d4fbf52d612140567e3bbf5
ni3rav <mahetan35@gmail.com>
2024-09-21 11:29:37 +0530

    cleaning navbar code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,25 @@ export default function Navbar() {
     setIsOpen(!isOpen);
   };
 
+  const navbarLinks = [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "LogIn",
+      href: "/login",
+    },
+    {
+      title: "TnC",
+      href: "/tnc",
+    },
+    {
+      title: "Github",
+      href: "https://github.com/shravanasati/everynyan",
+    },
+  ];
+
   return (
     <nav className="bg-zinc-900 shadow-md z-50 fixed sm:min-w-[50%] mt-8  rounded-2xl min-w-[80%]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -21,30 +40,21 @@ export default function Navbar() {
           </div>
           <div className="hidden md:block">
             <div className="ml-10 flex items-baseline space-x-4">
-              <Link
+              {navbarLinks.map((navbarLink, index) => (
+                <Link
+                  key={index}
+                  href={navbarLink.href}
+                  className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-xl font-medium"
+                >
+                  {navbarLink.title}
+                </Link>
+              ))}
+              {/* <Link
                 href="/"
                 className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-xl font-medium"
               >
                 Home
-              </Link>
-              <Link
-                href="/login"
-                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-xl font-medium"
-              >
-                LogIn
-              </Link>
-              <Link
-                href="/tnc"
-                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-xl font-medium"
-              >
-                TnC
-              </Link>
-              <Link
-                href="https://github.com/shravanasati/everynyan"
-                className="text-zinc-100 hover:text-zinc-400 px-3 py-2 rounded-md text-sm font-medium"
-              >
-                GitHub
-              </Link>
+              </Link> */}
             </div>
           </div>
           <div className="md:hidden">
@@ -66,30 +76,15 @@ export default function Navbar() {
       {isOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            <Link
-              href="/"
-              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-xl font-medium"
-            >
-              Home
-            </Link>
-            <Link
-              href="/login"
-              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-xl font-medium"
-            >
-              LogIn
-            </Link>
-            <Link
-              href="/tnc"
-              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-xl font-medium"
-            >
-              TnC
-            </Link>
-            <Link
-              href="/contact"
-              className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-xl font-medium"
-            >
-              Contact
-            </Link>
+            {navbarLinks.map((navbarLink, index) => (
+              <Link
+                key={index}
+                href={navbarLink.href}
+                className="text-zinc-100 hover:text-zinc-900 block px-3 py-2 rounded-md text-xl font-medium"
+              >
+                {navbarLink.title}
+              </Link>
+            ))}
           </div>
         </div>
       )}
