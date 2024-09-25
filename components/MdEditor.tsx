516034a84fac83268f11af7d0c077c1eb2cd8f7d
ni3rav <mahetan35@gmail.com>
2024-09-25 16:12:14 +0530

    adding toolbar in mdeditor component

--- components/MdEditor.tsx
+++ components/MdEditor.tsx
@@ -1,26 +1,31 @@
 "use client";
+import "@mdxeditor/editor/style.css";
+import {
+  MDXEditor,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  toolbarPlugin,
+} from "@mdxeditor/editor";
+import { string } from "zod";
 
-import { MDXEditor, MDXEditorMethods, headingsPlugin } from "@mdxeditor/editor";
-import { FC } from "react";
-
-interface EditorProps {
-  markdown: string;
-  editorRef?: React.MutableRefObject<MDXEditorMethods | null>;
-}
-
-/**
- * Extend this Component further with the necessary plugins or props you need.
- * proxying the ref is necessary. Next.js dynamically imported components don't support refs.
- */
-const MdEditor: FC<EditorProps> = ({ markdown, editorRef }) => {
+function MdEditor(props: { markdown: string }) {
+  const { markdown } = props;
   return (
     <MDXEditor
-      onChange={(e) => console.log(e)}
-      ref={editorRef}
       markdown={markdown}
-      plugins={[headingsPlugin()]}
+      plugins={[
+        toolbarPlugin({
+          toolbarContents: () => (
+            <>
+              {" "}
+              <UndoRedo />
+              <BoldItalicUnderlineToggles />
+            </>
+          ),
+        }),
+      ]}
     />
   );
-};
+}
 
 export default MdEditor;
