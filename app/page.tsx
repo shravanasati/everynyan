f50b8d82ad4b9d27b475ee376b57b025d4e13c54
ni3rav <mahetan35@gmail.com>
2024-10-30 12:11:50 +0530

    issue fixes on smaller device and made the blob better. might resume the section 2

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Section2 from "@/components/LandingPage/Section2";
 export default async function Home() {
   return (
     <div className="overflow-x-hidden flex-col justify-center items-center min-h-screen">
-      <BlobGradient className="absolute inset-0 md:-top-[300px] md:-left-[1100px] -top-[150px] -left-[150px] -z-50" />
+      <BlobGradient className="absolute inset-0 -z-50 md:h-[1000px] md::w-[1000px]" />
       <Section1 />
       <Section2 />
     </div>
