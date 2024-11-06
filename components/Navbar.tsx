3afe83b94bedfa98618c454f1b4aaac6709bc2e8
Shravan Asati <dev.shravan@protonmail.com>
2024-11-06 23:36:18 +0530

    implement logout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
+"use client"
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import SvgLogo from "@/components/SvgLogo";
 import { User } from "@/lib/user";
+import { logout } from "@/lib/actions/logout";
 
 export default function Navbar({user} : {user: User | null}) {
   const loggedIn = user !== null
@@ -15,14 +17,25 @@ export default function Navbar({user} : {user: User | null}) {
       {/* logo */}
 
       <div className="flex items-center space-x-4">
-        <Link href={loggedIn ? "/logout" : "/login"}>
+        {!loggedIn && (
+        <Link href="/login">
           <Button
             variant="ghost"
             className="hidden md:inline-flex hover:text-primary ease-ani font-bold"
           >
-            {loggedIn ? "Log Out" : "Log In"}
+            Log In
           </Button>
         </Link>
+        )}
+        {loggedIn && (
+          <Button
+            variant="ghost"
+            className="hidden md:inline-flex hover:text-primary ease-ani font-bold"
+            onClick={async () => {await logout()}}
+          >
+            Log Out
+          </Button>
+        )}
         <Link href="/create">
           <Button className="bg-primary text-primary-foreground hover:bg-primary/90 ease-ani font-bold">
             Explore {/* add create link once done */}
