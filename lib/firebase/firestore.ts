2787b23f1a53a990aff8b9f845558505c8a34751
Shravan Asati <dev.shravan@protonmail.com>
2024-11-06 21:36:23 +0530

    refactor import path

--- lib/firebase/firestore.ts
+++ lib/firebase/firestore.ts
@@ -7,7 +7,7 @@ import {
 } from "firebase/firestore";
 
 import { db } from "@/lib/firebase/app";
-import { generatePostID } from "../utils";
+import { generatePostID } from "@/lib/utils";
 
 export type OTPEntry = {
   otp: string;
