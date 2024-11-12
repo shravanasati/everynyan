ad4db42d578e132f299d1521f54252342e2d491f
ni3rav <mahetan35@gmail.com>
2024-11-12 10:54:31 +0530

    adding dialog component for modal and removing sidebards as of now

--- components/Posts/PostView/PostView.tsx
+++ components/Posts/PostView/PostView.tsx
@@ -12,12 +12,12 @@ async function PostView({ postID, isAdmin }: { postID: string, isAdmin: boolean 
   return (
     <main className="min-h-[92vh] grid grid-cols-1 md:grid-cols-5 grid-rows-1 gap-2">
       {/* !!right sidebar */}
-      <div className="bg-red-400 md:block hidden">left sidebar</div>
+      {/* <div className="bg-red-400 md:block hidden">left sidebar</div> */}
       {/* !!right sidebar */}
 
       {/* post and comment components */}
-      <div className="md:col-span-3">
-        <div className="grid grid-cols-1 grid-rows-6 md:grid-rows-5 gap-2 h-full">
+      <div className="md:col-span-5">
+        <div className="grid grid-cols-1 grid-rows-6 md:grid-rows-5 gap-2 h-full px-1 md:px-4">
           {/* #post */}
           <div className="row-span-3 md:row-span-2 md:p-0 px-2 py-3">
             <PerPost boardName={post.board} content={post.body} title={post.title} upVotes={post.upvotes} downVotes={post.downvotes} id={post.id} />
@@ -33,9 +33,9 @@ async function PostView({ postID, isAdmin }: { postID: string, isAdmin: boolean 
       </div>
       {/* post and comment components */}
       {/* !!left sidebar */}
-      <div className="bg-blue-400 md:col-start-5 md:block hidden">
+      {/* <div className="bg-blue-400 md:col-start-5 md:block hidden">
         right sidebar
-      </div>
+      </div> */}
       {/* !!left sidebar */}
     </main>
   );
