bfa48a088f2fce208c3dd694f208134ca9d0d31a
ni3rav <mahetan35@gmail.com>
2024-12-11 11:42:24 +0530

    minor fixes

--- components/AskNotificationPerm.tsx
+++ components/AskNotificationPerm.tsx
@@ -1,36 +1,46 @@
-"use client"
+"use client";
 
-import { useToast } from "@/hooks/use-toast"
-import { useNotification } from "@/hooks/useNotification"
-import { Button } from "./ui/button"
-import { useEffect } from "react"
+import { useToast } from "@/hooks/use-toast";
+import { useNotification } from "@/hooks/useNotification";
+import { Button } from "./ui/button";
+import { useEffect } from "react";
 
 export function AskNotificationPerm() {
-	const { isDenied, isGranted, isSubscribed, isSupported, handleSubscribe } = useNotification()
-	const { toast } = useToast()
+  const { isDenied, isGranted, isSubscribed, isSupported, handleSubscribe } =
+    useNotification();
+  const { toast } = useToast();
 
-	useEffect(() => {
-		if (!isSupported || isSubscribed || isGranted || isDenied) {
-			return
-		}
-		const alreadyAsked = sessionStorage.getItem("alreadyAsked")
-		if (alreadyAsked) {
-			return
-		}
+  useEffect(() => {
+    if (!isSupported || isSubscribed || isGranted || isDenied) {
+      return;
+    }
+    const alreadyAsked = sessionStorage.getItem("alreadyAsked");
+    if (alreadyAsked) {
+      return;
+    }
 
-		toast({
-			title: "Notification Permission",
-			description: "Be in-the-loop with our latest updates by enabling notifications.",
-			action: (
-				<Button variant="outline" size="sm" className="gap-1" onClick={handleSubscribe}> Subscribe </Button>
-			)
-		})
-		sessionStorage.setItem("alreadyAsked", "true")
-	}, [isSupported, isSubscribed, isGranted, isDenied, handleSubscribe, toast])
+    toast({
+      title: "Notification Permission",
+      description:
+        "Be in-the-loop with our latest updates by enabling notifications.",
+      action: (
+        <Button
+          variant="outline"
+          size="sm"
+          className="gap-1 border border-primary/90 hover:bg-primary/80 hover:text-black"
+          onClick={handleSubscribe}
+        >
+          {" "}
+          Subscribe{" "}
+        </Button>
+      ),
+    });
+    sessionStorage.setItem("alreadyAsked", "true");
+  }, [isSupported, isSubscribed, isGranted, isDenied, handleSubscribe, toast]);
 
-	return (
-		<div className="hidden">
-			Current Status: {isDenied ? "Denied" : isGranted ? "Granted" : "Unknown"}
-		</div>
-	)
+  return (
+    <div className="hidden">
+      Current Status: {isDenied ? "Denied" : isGranted ? "Granted" : "Unknown"}
+    </div>
+  );
 }
