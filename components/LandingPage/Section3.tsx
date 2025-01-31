0cc37207db27bdf1e107bb612305865dbbafc6cc
Shravan Asati <dev.shravan@protonmail.com>
2025-01-31 20:46:40 +0530

    fix typos in section3

--- components/LandingPage/Section3.tsx
+++ components/LandingPage/Section3.tsx
@@ -5,31 +5,31 @@ import { motion } from "framer-motion";
 const testimonials = [
   {
     quote:
-      "A feed that curates content across all boards and gives you a glimpse of what's happening in the community while respecting your short lived attention span",
+      "A feed that curates content across all boards and gives you a glimpse of what's happening in the community while respecting your short lived attention span.",
     name: "Feed",
     src: "/parallax/feed.jpg",
   },
   {
     quote:
-      "Post your thoughts, questions, or anything you want to share with the community with complete anonimity and no feaer of being judged",
+      "Post your thoughts, questions, or anything you want to share with the community with complete anonymity without the fear of being judged.",
     name: "Posts",
     src: "/parallax/create.jpg",
   },
   {
     quote:
-      "Interact with posts by upvoting or downvoting them and again with complete anonimity. No one will know what you upvoted or downvoted",
+      "Interact with posts by upvoting or downvoting them. No one will know what you upvoted or downvoted.",
     name: "Interact",
     src: "/parallax/updownvote.jpg",
   },
   {
     quote:
-      "Comment on posts and interact with the community. Again, with complete anonimity. No one will know who you are",
+      "Comment on posts and interact with the community. No one will know who you are.",
     name: "Comments",
     src: "/parallax/comments.jpg",
   },
   {
     quote:
-      "Get notified when someone interacts with your posts or comments. Get notified when someone comments on your posts and yes you are still anonymous",
+      "Get notified when someone interacts with your posts or comments.",
     name: "Notifications",
     src: "/parallax/notifications.jpg",
   },
@@ -62,7 +62,7 @@ export default function Section3() {
       >
         EveryNyan has....
       </motion.h2>
-      <AnimatedTestimonials testimonials={testimonials} autoplay={false} />
+      <AnimatedTestimonials testimonials={testimonials} autoplay={true} />
     </div>
   );
 }
